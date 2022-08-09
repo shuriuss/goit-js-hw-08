6b1fa374837d972998d84f7c42905532d40a5775
shuriuss <104681622+shuriuss@users.noreply.github.com>
2022-08-09 20:15:57 +0300

    add task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(getPauseTime, 1000));
+function getPauseTime({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
+const startTime = localStorage.getItem('videoplayer-current-time');
+
+if (startTime) {
+  player.setCurrentTime(startTime);
+}
